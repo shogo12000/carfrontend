5d106811c0439139f3f41d758a255791c32f2350
sidney <shogo12000@hotmail.com>
2025-11-11 00:06:40 -0800

    Configure Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react' 
 import './App.css'
 
 function App() {
@@ -8,7 +6,9 @@ function App() {
 
   return (
     <>
- 
+   <h1 class="text-3xl font-bold underline">
+    Hello world!x
+  </h1>
     </>
   )
 }
