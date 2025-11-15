73bec5213cd001d74926ae6413fc82065e18d99d
sidney <shogo12000@hotmail.com>
2025-11-14 22:04:02 -0800

    fix: fetch api Login

--- src/utils/LoginForm.jsx
+++ src/utils/LoginForm.jsx
@@ -14,7 +14,7 @@ export default function LoginForm({ ChangeForm }) {
     console.log(loginForm);
 
     try {
-      const res = await fetch("http://localhost:3000/cars/login", {
+      const res = await fetch("https://project-car-back-end.vercel.app/cars/login", {
         method: "POST",
         credentials: "include",
         headers: {
