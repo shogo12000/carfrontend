4bbc4bc53be3d1ca5518297bd1a662dba9b86c9d
sidney <shogo12000@hotmail.com>
2025-11-18 12:49:11 -0800

    feat: show image from cloudinary

--- src/utils/local.jsx
+++ src/utils/local.jsx
@@ -1,16 +1,16 @@
-export const fetchCheckUserLogin = "http://localhost:3000/cars/userstatus";
-export const fetchLogout = "http://localhost:3000/cars/logout";
-export const logForm = "http://localhost:3000/cars/login";
-export const regForm = "http://localhost:3000/cars/register";
-export const allCars = "http://localhost:3000/cars/cars";
-export const addCars = "http://localhost:3000/cars/addcar";
-export const userCars = "http://localhost:3000/cars/usercar";
+// export const fetchCheckUserLogin = "http://localhost:3000/cars/userstatus";
+// export const fetchLogout = "http://localhost:3000/cars/logout";
+// export const logForm = "http://localhost:3000/cars/login";
+// export const regForm = "http://localhost:3000/cars/register";
+// export const allCars = "http://localhost:3000/cars/cars";
+// export const addCars = "http://localhost:3000/cars/addcar";
+// export const userCars = "http://localhost:3000/cars/usercar";
  
 
-// export const fetchCheckUserLogin = "https://project-car-back-end.vercel.app/cars/userstatus";
-// export const fetchLogout = "https://project-car-back-end.vercel.app/cars/logout";
-// export const logForm = "https://project-car-back-end.vercel.app/cars/login";
-// export const regForm = "https://project-car-back-end.vercel.app/cars/register";
-// export const allCars = "https://project-car-back-end.vercel.app/cars/cars";
-// export const addCars = "https://project-car-back-end.vercel.app/cars/addcar";
-// export const userCars = "https://project-car-back-end.vercel.app/cars/usercar";
+export const fetchCheckUserLogin = "https://project-car-back-end.vercel.app/cars/userstatus";
+export const fetchLogout = "https://project-car-back-end.vercel.app/cars/logout";
+export const logForm = "https://project-car-back-end.vercel.app/cars/login";
+export const regForm = "https://project-car-back-end.vercel.app/cars/register";
+export const allCars = "https://project-car-back-end.vercel.app/cars/cars";
+export const addCars = "https://project-car-back-end.vercel.app/cars/addcar";
+export const userCars = "https://project-car-back-end.vercel.app/cars/usercar";
